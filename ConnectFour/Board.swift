792ed09d7c910875a6f744624e102f756c8bfef7
Shalom Yemane <yemaneshalom@gmail.com>
2020-05-11 20:27:14 -0700

    first commit

---
+++ ConnectFour/Board.swift
@@ -0,0 +1,14 @@
+//
+//  Board.swift
+//  ConnectFour
+//
+//  Created by CSUFTitan on 5/11/20.
+//  Copyright © 2020 CSUFTitan. All rights reserved.
+//
+
+import UIKit
+
+class Board: NSObject {
+    static var width = 7
+    static var height = 6
+}
---
+++ ConnectFour/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ConnectFour
+//
+//  Created by CSUFTitan on 5/11/20.
+//  Copyright © 2020 CSUFTitan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var columnButtons: [UIButton]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func makeMove(_ sender: UIButton) {
+    }
+    
+}
+
